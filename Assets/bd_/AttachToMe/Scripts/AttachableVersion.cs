fcbb0424ebce82dd2d9edb899f718c1b742f6a3c
bd_ <bdunderscore@fushizen.net>
2021-07-13 19:34:57 -0700

    Add version display to tutorial UI
    Closes #16

---
+++ Assets/bd_/AttachToMe/Scripts/AttachableVersion.cs
@@ -0,0 +1,24 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace net.fushizen.attachable
+{
+
+    public class AttachableVersion : UdonSharpBehaviour
+    {
+        readonly string VERSION = "AttachToMe v0.9.8alpha";
+
+        void Start()
+        {
+            var uiText = GetComponent<UnityEngine.UI.Text>();
+
+            uiText.text = VERSION;
+
+            enabled = false;
+        }
+    }
+
+}
