ab0a7950f012577c62bfebc0dbfa189adc54744c
bd_ <bdunderscore@fushizen.net>
2022-03-05 15:48:15 -0800

    Update version

--- Assets/bd_/AttachToMe/Scripts/AttachableVersion.cs
+++ Assets/bd_/AttachToMe/Scripts/AttachableVersion.cs
@@ -24,7 +24,7 @@ namespace net.fushizen.attachable
 
     public class AttachableVersion : UdonSharpBehaviour
     {
-        readonly string VERSION = "AttachToMe vDEV";
+        readonly string VERSION = "AttachToMe v1.1.2";
 
         void Start()
         {
